aee41bac296eece9c30565c5824db9a019833ee0
Bagrat Aznauryan <bagrat@aznauryan.org>
2016-08-18 04:24:44 +0400

    Add decorator docs

--- calm/decorator.py
+++ calm/decorator.py
@@ -1,7 +1,23 @@
+"""
+This module defines general decorators to define the Calm Application.
+"""
+from calm.resource import Resource
+from calm.ex import DefinitionError
 
 
 def produces(resource_type):
+    """Decorator to specify what kind of Resource the handler produces."""
+    if not isinstance(resource_type, Resource):
+        raise DefinitionError('@produces value should be of type Resource.')
+
     def decor(func):
+        """
+        The function wrapper.
+
+        It checks whether the function is already defined as a Calm handler or
+        not and sets the appropriate attribute based on that. This is done in
+        order to not enforce a particular order for the decorators.
+        """
         if getattr(func, 'handler_def', None):
             func.handler_def.produces = resource_type
         else:
@@ -13,7 +29,18 @@ def produces(resource_type):
 
 
 def consumes(resource_type):
+    """Decorator to specify what kind of Resource the handler consumes."""
+    if not isinstance(resource_type, Resource):
+        raise DefinitionError('@consumes value should be of type Resource.')
+
     def decor(func):
+        """
+        The function wrapper.
+
+        It checks whether the function is already defined as a Calm handler or
+        not and sets the appropriate attribute based on that. This is done in
+        order to not enforce a particular order for the decorators.
+        """
         if getattr(func, 'handler_def', None):
             func.handler_def.consumes = resource_type
         else:
